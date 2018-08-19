254b2874a132f1de37c7d391547c44891867223d
hochenchong <hochenchong@163.com>
2018-08-19 22:49:22 +0800

    服务监控 Hystrix Dashboard 学习

---
+++ microservicecloud/microservicecloud-consumer-hystrix-dashboard/src/main/java/hochenchong/springcloud/DeptConsumer_DashBoard_App.java
@@ -0,0 +1,13 @@
+package hochenchong.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard // 启动服务监控 hystrix Dashboard，服务提供者需要引入 actuator 依赖
+public class DeptConsumer_DashBoard_App {
+	public static void main(String[] args) {
+		SpringApplication.run(DeptConsumer_DashBoard_App.class, args);
+	}
+}
