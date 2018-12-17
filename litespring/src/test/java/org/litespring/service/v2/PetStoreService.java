3ec04ad9354c80b1b3d10e8ca837a30ddf988aba
hochenchong <hochenchong@163.com>
2018-12-17 20:57:56 +0800

    实现 setter 注入的前期准备

---
+++ litespring/src/test/java/org/litespring/service/v2/PetStoreService.java
@@ -0,0 +1,36 @@
+package org.litespring.service.v2;
+
+import org.litespring.dao.v2.AccountDao;
+import org.litespring.dao.v2.ItemDao;
+
+public class PetStoreService {
+	private AccountDao accountDao;
+	private ItemDao itemDao;
+	private String owner;
+	private int version;
+	
+	public AccountDao getAccountDao() {
+		return accountDao;
+	}
+	public ItemDao getItemDao() {
+		return itemDao;
+	}
+	public String getOwner() {
+		return owner;
+	}
+	public int getVersion() {
+		return version;
+	}
+	public void setAccountDao(AccountDao accountDao) {
+		this.accountDao = accountDao;
+	}
+	public void setItemDao(ItemDao itemDao) {
+		this.itemDao = itemDao;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	public void setVersion(int version) {
+		this.version = version;
+	}
+}
