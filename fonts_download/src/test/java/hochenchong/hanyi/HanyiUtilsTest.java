e475f056bd5a649e5de4ee057948cc6b9e8a5a46
hochenchong <hochenchong@163.com>
2019-05-13 20:57:03 +0800

    获取汉仪字体信息相关的 List 测试方法编写

--- fonts_download/src/test/java/hochenchong/hanyi/HanyiUtilsTest.java
+++ fonts_download/src/test/java/hochenchong/hanyi/HanyiUtilsTest.java
@@ -3,13 +3,27 @@ package hochenchong.hanyi;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.List;
 
 public class HanyiUtilsTest {
 
+    /**
+     * 获取汉仪字体信息相关的 json 数据
+     */
     @Test
-    public void getHanyiFontsInfoJsonStr() {
-        String hanyiFontsInfoJsonStr = HanyiUtils.getHanyiFontsInfoJsonStr();
+    public void getFontsInfoJsonStr() {
+        String hanyiFontsInfoJsonStr = HanyiUtils.getFontsInfoJsonStr();
+        // 简单的判断是否为 null
         Assert.assertNotNull(hanyiFontsInfoJsonStr);
+    }
+
+    /**
+     * 获取汉仪字体信息相关的 List
+     */
+    @Test
+    public void testJsonToBean() {
+        List<HanyiBean> hanyiBeans = HanyiUtils.getFontsInfoBeans();
+        // 简单的判断是否为 null
+        Assert.assertNotNull(hanyiBeans);
     }
 }
