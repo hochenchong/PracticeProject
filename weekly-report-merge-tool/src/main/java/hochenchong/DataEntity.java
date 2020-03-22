a2f4a5e8e02bf815df4a37d8d83a81a6eccd2189
hochenchong <30753404+hochenchong@users.noreply.github.com>
2020-03-23 00:31:03 +0800

    Excel 第一列新增 “项目”

--- weekly-report-merge-tool/src/main/java/hochenchong/DataEntity.java
+++ weekly-report-merge-tool/src/main/java/hochenchong/DataEntity.java
@@ -3,7 +3,6 @@ package hochenchong;
 import com.alibaba.excel.annotation.ExcelProperty;
 import com.alibaba.excel.annotation.format.DateTimeFormat;
 import com.alibaba.excel.annotation.write.style.ColumnWidth;
-import com.alibaba.excel.annotation.write.style.ContentRowHeight;
 import com.alibaba.excel.annotation.write.style.HeadRowHeight;
 import lombok.Data;
 
@@ -11,32 +10,35 @@ import java.util.Date;
 
 @Data
 @HeadRowHeight(30)
-@ContentRowHeight(30)
+// @ContentRowHeight(30)
 @ColumnWidth(25)
 public class DataEntity {
+    // 项目
+    @ExcelProperty(value = "项目", index = 0)
+    private String project;
     // 开发人员
-    @ExcelProperty(value = "姓名", index = 0)
+    @ExcelProperty(value = "姓名", index = 1)
     private String developer;
     // 项目工作
-    @ColumnWidth(70)
-    @ExcelProperty(value = "项目工作", index = 1)
+    @ColumnWidth(85)
+    @ExcelProperty(value = "项目工作", index = 2)
     private String projectWork;
     // 需求提出人
-    @ExcelProperty(value = "需求提出人", index = 2)
+    @ExcelProperty(value = "需求提出人", index = 3)
     private String demander;
     // 计划开始时间
     @DateTimeFormat("yyyy/MM/dd")
-    @ExcelProperty(value = "计划开始时间", index = 3)
+    @ExcelProperty(value = "计划开始时间", index = 4)
     private Date startDate;
     // 计划结束时间
     @DateTimeFormat("yyyy/MM/dd")
-    @ExcelProperty(value = "计划完成时间", index = 4)
+    @ExcelProperty(value = "计划完成时间", index = 5)
     private Date endDate;
     // 完成状态
-    @ExcelProperty(value = "完成状态", index = 5)
+    @ExcelProperty(value = "完成状态", index = 6)
     private String status;
     // 问题反馈
-    @ExcelProperty(value = "问题反馈", index = 6)
+    @ExcelProperty(value = "问题反馈", index = 7)
     private String feedback;
 }
 
