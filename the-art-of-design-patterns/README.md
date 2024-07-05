aed00bebbcb17e53a2d9b83925f1743260bfc73e
hochenchong <hochenchong@gmail.com>
2024-07-05 12:55:55 +0800

    新建《设计模式的艺术》模块； 单例模式提交

---
+++ the-art-of-design-patterns/README.md
@@ -0,0 +1,31 @@
+## 设计模式的艺术
+
+### 前言
+
+
+---
+
+### 创建型设计模式
+
+#### 单例模式
+* [饿汉式单例：EagerSingleton](./src/pattern01/EagerSingleton.java)
+* [懒汉式单例：LazySingleton](./src/pattern01/LazySingleton.java)
+* [IoDH 代理，静态内部类：Singleton](./src/pattern01/Singleton.java)
+
+
+---
+
+### 结构型设计模式
+
+
+---
+
+### 行为型设计模式
+
+---
+
+### 后记
+
+开始时间：2024-07-04
+
+结束时间：
---
+++ the-art-of-design-patterns/src/pattern01/LazySingleton.java
@@ -0,0 +1,27 @@
+package pattern01;
+
+/**
+ * 懒汉式单例
+ *     获取时，再判断是否为空，为空则初始化，避免浪费
+ *     存在问题，每次都要非空判断
+ *     volatile 屏蔽 JVM 的代码优化
+ *
+ * @author hochenchong
+ * @date 2024/7/5
+ */
+public class LazySingleton {
+    // 确保多个线程都能观测到变化
+    private volatile static LazySingleton instance;
+
+    public static LazySingleton getInstance() {
+        // 双重检查锁定
+        if (instance == null) {
+            synchronized (LazySingleton.class) {
+                if (instance == null) {
+                    instance = new LazySingleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
