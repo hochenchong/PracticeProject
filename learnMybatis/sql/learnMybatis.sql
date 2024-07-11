ff8f399f20c7c26743cdcfa431b4deb65b9c0959
hochenchong <30753404+hochenchong@users.noreply.github.com>
2024-07-11 17:14:19 +0800

    Delete learnMybatis directory

--- learnMybatis/sql/learnMybatis.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE tbl_employee (
-	id INT(11) PRIMARY KEY AUTO_INCREMENT,
-	last_name VARCHAR(255),
-	gender CHAR(1),
-	email VARCHAR(255)
-)
--- learnMybatis/src/main/java/hochenchong/mybatis01/dao/EmployeeMapper.java
+++
@@ -1,8 +0,0 @@
-package hochenchong.mybatis01.dao;
-
-import hochenchong.mybatis01.bean.Employee;
-
-public interface EmployeeMapper {
-	
-	Employee selectEmp(int id);
-}
